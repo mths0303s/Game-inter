4785beee0f269f98df55dbd188689c7d171d4256
mths0303s <matheusaraujo1460@gmail.com>
2024-10-01 12:14:25 -0300

    Corrigindo colisão fire word-céu

--- Scripts/fire area.gd
+++ Scripts/fire area.gd
@@ -4,8 +4,7 @@ extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	collision.shape.size = fire.get_rect().size
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
